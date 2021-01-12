38965a8733cd6a656c87449da91289184058e585
Dmitriy <fed3375546@yandex.ru>
2021-01-12 20:26:37 +0300

    Initial commit

---
+++ src/main/java/ru/fed3375546/MobileAppMsApplication.java
@@ -0,0 +1,13 @@
+package ru.fed3375546;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fed3375546/MobileAppMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fed3375546;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
